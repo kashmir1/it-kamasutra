ae71d598b1ff464ab221011561963d7811426392
Alex <lzlin@mail.ru>
2021-07-27 22:59:10 +1000

    создает контейнерные компоненты и создает контекс

---
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import classes from './Dialogs.module.css'
+import Message from "./Message/Message";
+import DialogsItem from "./DialogItem/DialogsItem";
+import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/dialog-reducer";
+
+const Dialogs = (props) => {
+
+    let state = props.store.getState().messagesPage;
+
+    let dialogsElements = state.dialogs.map(d =>
+        <DialogsItem name={d.name} id={d.id}/>);
+
+    let messagesElements = state.messages.map(m => <Message message={m.message}/>);
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        props.store.dispatch(addMessageActionCreator())
+    };
+
+    let onMessageChange = (e) => {
+        let text = e.target.value;
+        props.store.dispatch(updateNewMessageTextActionCreator(text))
+    };
+
+    return (
+        <div className={classes.dialogs}>
+            <div><textarea
+                ref={newMessageElement}
+                onChange={onMessageChange}
+                value={props.newPostMessage}
+            />
+            </div>
+            <div>
+                <button onClick={addMessage}>New message</button>
+            </div>
+            <div className={classes.dialogsItem}>
+                {dialogsElements}
+            </div>
+            <div className={classes.messages}>
+                {messagesElements}
+            </div>
+        </div>
+    )
+};
+
+export default Dialogs
