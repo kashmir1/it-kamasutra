2e60353ff79eb49adbca0e19bbc4f1b73e08ffe9
kashmir1 <lzlin@mail.ru>
2021-08-27 15:41:31 +1000

    добавляет фолой анфолоу запросы

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from "./Users.module.css";
 import userPhoto from "../../assets/img/user.png";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -34,10 +35,33 @@ let Users = (props) => {
                 <div>
                 {u.followed
                     ? <button onClick={() => {
-                        props.unfollow(u.id)
+                        axios
+                            .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                withCredentials: true,
+                                headers: {
+                                    "API-KEY": '2ffdfc84-666a-4bdd-8ed9-1dc48a38d879'
+                                }
+                            })
+                            .then(response => {
+                                if (response.data.resultCode === 0) {
+                                    props.unfollow(u.id)
+                                }
+                            });
                     }}>Unfollow</button>
                     : <button onClick={() => {
-                        props.follow(u.id)
+
+
+                        axios
+                            .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: {
+                                    "API-KEY": '2ffdfc84-666a-4bdd-8ed9-1dc48a38d879'
+                                }                            })
+                            .then(response => {
+                                if (response.data.resultCode === 0) {
+                                    props.follow(u.id)
+                                }
+                            });
                     }}>Follow</button>}
                 </div>
                 </span>
