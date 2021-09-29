878b5781d284a8b02360cb6c9b7d4776f6c673dd
Alex <lzlin@mail.ru>
2021-09-29 22:42:08 +1000

    делает форму логина и подключает редакс форму

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+import axios from "axios";
 
-const Login = (props) => {
+const LoginForm = (props) => {
     return (
         <div>
-            <h1>Login</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div><Field component={'input'} name={'login'} placeholder={'login'} /></div>
+                <div><Field component={'input'} name={'password'} placeholder={'password'} /></div>
+                <div><Field component={'input'} type={'checkbox'} name={'rememberMe'} />Remember me</div>
+                <div><button>Login</button></div>
+            </form>
         </div>
     )
 };
+
+
+const LoginReduxForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm);
+
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+        axios
+            .post('https://social-network.samuraijs.com/api/1.0/auth/login', formData)
+            .then(response => {
+                return response.data
+            })
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
+    )
+};
+
 
 export default Login;
